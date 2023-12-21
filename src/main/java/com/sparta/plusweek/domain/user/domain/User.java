c7971320b66a2002846b4b7c15d9b0fe83b2ec33
ghkdrbwjd369 <ghkdrbwjd369@naver.com>
2023-12-21 17:56:39 +0900

    feature: add User Entity

---
+++ src/main/java/com/sparta/plusweek/domain/user/domain/User.java
@@ -0,0 +1,32 @@
+package com.sparta.plusweek.domain.user.domain;
+
+import com.sparta.plusweek.domain.model.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "user")
+public class User extends BaseEntity {
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = false)
+    private String nickname;
+
+    @Builder
+    public User(String username, String password, String nickname) {
+        this.username = username;
+        this.password = password;
+        this.nickname = nickname;
+    }
+}
