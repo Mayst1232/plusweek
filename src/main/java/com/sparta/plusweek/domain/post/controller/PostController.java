b4643de5dc410a7ab68e83308ab61e4ceef486ad
ghkdrbwjd369 <ghkdrbwjd369@naver.com>
2023-12-22 15:25:39 +0900

    feat: add deletePost Controller API

--- src/main/java/com/sparta/plusweek/domain/post/controller/PostController.java
+++ src/main/java/com/sparta/plusweek/domain/post/controller/PostController.java
@@ -17,6 +17,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -87,4 +88,14 @@ public class PostController {
             .build());
     }
 
+    @DeleteMapping("post/{id}")
+    public ResponseEntity<?> deletePost(@PathVariable Long id,
+        @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        postService.deletePost(id, userDetails.getUser());
+
+        return ResponseEntity.ok(RootResponseDto.builder()
+            .code("200")
+            .message("게시물 삭제 성공")
+            .build());
+    }
 }
