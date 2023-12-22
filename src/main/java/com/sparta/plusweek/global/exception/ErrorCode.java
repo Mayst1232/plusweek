5077ef5c9c04940b5674f74db8a82d122d0ccdd0
ghkdrbwjd369 <ghkdrbwjd369@naver.com>
2023-12-22 14:09:57 +0900

    feat: add Some Exception ErrorCode

--- src/main/java/com/sparta/plusweek/global/exception/ErrorCode.java
+++ src/main/java/com/sparta/plusweek/global/exception/ErrorCode.java
@@ -12,7 +12,10 @@ public enum ErrorCode {
     SIGNUP_FAILED(HttpStatus.BAD_REQUEST, "1001", "회원가입에 실패했습니다."),
     PASSWORD_NICKNAME(HttpStatus.BAD_REQUEST, "1002", "비밀번호에 닉네임이 들어갈 수 없습니다."),
     VALID_PASS_INCORRECT(HttpStatus.BAD_REQUEST, "1003", "비밀번호와 확인 비밀번호가 다릅니다."),
-    DUPLICATE_USERNAME(HttpStatus.BAD_REQUEST, "1004", "중복된 유저 이름 입니다.");
+    DUPLICATE_USERNAME(HttpStatus.BAD_REQUEST, "1004", "중복된 유저 이름 입니다."),
+
+    CREATE_POST_FAIL(HttpStatus.BAD_REQUEST, "2000", "게시글 작성에 실패했습니다."),
+    NOT_EXIST_POST(HttpStatus.BAD_REQUEST, "2001", "해당 ID의 게시글이 존재하지 않습니다.");
 
     private final HttpStatus status;
     private final String code;
