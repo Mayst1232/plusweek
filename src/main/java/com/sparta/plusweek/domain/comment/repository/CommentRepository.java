61e8347121f36caf0335f7b1486f2d4c3cf44f56
ghkdrbwjd369 <ghkdrbwjd369@naver.com>
2023-12-22 16:53:54 +0900

    feature: add findAllByPost_Id & findByIdAndUser_Id

--- src/main/java/com/sparta/plusweek/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/sparta/plusweek/domain/comment/repository/CommentRepository.java
@@ -1,8 +1,15 @@
 package com.sparta.plusweek.domain.comment.repository;
 
 import com.sparta.plusweek.domain.comment.domain.Comment;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
+    List<Comment> findAllByPost_Id(Long postId, Pageable pageable);
+
+
+    Optional<Comment> findByIdAndUser_id(Long commentId, Long userId);
 }
