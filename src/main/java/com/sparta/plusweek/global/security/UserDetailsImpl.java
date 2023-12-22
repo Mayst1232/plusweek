4288fa42ecedffa587c2097ee2fb256d2f9ff2ce
ghkdrbwjd369 <ghkdrbwjd369@naver.com>
2023-12-22 09:13:26 +0900

    feat: add UserDetails for get user who authenticate & authorize complete

---
+++ src/main/java/com/sparta/plusweek/global/security/UserDetailsImpl.java
@@ -0,0 +1,54 @@
+package com.sparta.plusweek.global.security;
+
+import com.sparta.plusweek.domain.user.domain.User;
+import java.util.Collection;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private final User user;
+
+    public UserDetailsImpl(User user) {
+        this.user = user;
+    }
+
+    public User getUser(){
+        return user;
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
