c7a3362490b3c0fe7f31030b008b3cc853b1a80d
ghkdrbwjd369 <ghkdrbwjd369@naver.com>
2023-12-22 15:36:22 +0900

    feat: add Comment Entity

---
+++ src/main/java/com/sparta/plusweek/domain/comment/domain/Comment.java
@@ -0,0 +1,40 @@
+package com.sparta.plusweek.domain.comment.domain;
+
+import com.sparta.plusweek.domain.model.BaseEntity;
+import com.sparta.plusweek.domain.post.domain.Post;
+import com.sparta.plusweek.domain.user.domain.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "comment")
+public class Comment extends BaseEntity {
+
+    @Column(nullable = false, length = 500)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @Builder
+    public Comment(String content, User user, Post post) {
+        this.content = content;
+        this.user = user;
+        this.post = post;
+    }
+}
