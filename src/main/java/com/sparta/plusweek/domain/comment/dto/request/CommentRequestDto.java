f99c862c74a02abf1ec73558386b320a771c19c5
ghkdrbwjd369 <ghkdrbwjd369@naver.com>
2023-12-22 16:14:31 +0900

    fix: number can't use NotEmpty or NotBlank

--- src/main/java/com/sparta/plusweek/domain/comment/dto/request/CommentRequestDto.java
+++ src/main/java/com/sparta/plusweek/domain/comment/dto/request/CommentRequestDto.java
@@ -1,13 +1,14 @@
 package com.sparta.plusweek.domain.comment.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
 public record CommentRequestDto(
 
-    @NotBlank
+    @NotNull
     Long postId,
     @NotBlank
     @Size(max = 500)
